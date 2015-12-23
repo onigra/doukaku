0cace5bb2a318263bac117ea96f6b9a322ecc6b3
onigra <3280467rec@gmail.com>
2015-12-23 20:18:31 +0900

    メソッド切り出す

--- tick_tack_toe/tick_tack_toe.rb
+++ tick_tack_toe/tick_tack_toe.rb
@@ -23,40 +23,54 @@ class TickTackToe
     @result
   end
 
+  private
+
   def game
     @input.scan(/[1-9]/).each_with_index do |item, idx|
       break if idx == 8
       idx = idx + 1
 
-      if @keika.include?(item)
-        if (idx % 2) == 0
-          @result = "Foul : o won."
-          break
-        else
-          @result = "Foul : x won."
-          break
-        end
-      else
-        @keika << item
-      end
-
-      if (idx % 2) == 0
-        @koukou << item.to_i
-
-        if WIN_PATTERN.map { |i| @koukou.sort.join =~ /#{i}/ }.any? { |w| w != nil }
-          @result = "x won."
-          break
-        end
-      else
-        @senkou << item.to_i
-
-        if WIN_PATTERN.map { |i| @senkou.sort.join =~ /#{i}/ }.any? { |w| w != nil }
-          @result = "o won."
-          break
-        end
-      end
+      break if foul_check(idx, item)
+      break if win_check(idx, item)
     end
 
     @result = "Draw game." if @result.nil?
+  end
+
+  def foul_check(idx, item)
+    if @keika.include?(item)
+      if (idx % 2) == 0
+        @result = "Foul : o won."
+        true
+      else
+        @result = "Foul : x won."
+        true
+      end
+    else
+      @keika << item
+      false
+    end
+  end
+
+  def win_check(idx, item)
+    if (idx % 2) == 0
+      @koukou << item.to_i
+
+      if WIN_PATTERN.map { |i| @koukou.sort.join =~ /#{i}/ }.any? { |w| w != nil }
+        @result = "x won."
+        true
+      else
+        false
+      end
+    else
+      @senkou << item.to_i
+
+      if WIN_PATTERN.map { |i| @senkou.sort.join =~ /#{i}/ }.any? { |w| w != nil }
+        @result = "o won."
+        true
+      else
+        false
+      end
+    end
   end
 end
